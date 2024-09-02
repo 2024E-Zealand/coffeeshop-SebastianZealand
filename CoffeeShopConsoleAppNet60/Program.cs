4e189a8c9fbdeaa7a7a95186c2bd3f0ccad84390
SebastianZealand <zealand@tbrinch.com>
2024-09-02 15:22:35 +0200

    Done with task 8. Created a list for coffe with milk.

--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        BlackCoffee blackCoffee = new BlackCoffee();
-        Latte latte = new Latte();
-        Cortado cortado = new Cortado();
+        BlackCoffee blackCoffee = new BlackCoffee(0);
+        Latte latte = new Latte(0);
+        Cortado cortado = new Cortado(0);
 
         List<Coffee> coffees = new List<Coffee>();
         
@@ -18,5 +18,12 @@ class Program
         {
             Console.WriteLine(c);
         }
+        
+        // Create list of coffee containing milk, so list of classes implementing the IMilk
+        List<IMilk> coffeeWithMilk = new List<IMilk>();
+        
+        coffeeWithMilk.Add(latte);
+        coffeeWithMilk.Add(cortado);
+        
     }
 }
